3e87a2e296647ad570e657e1452770a7d509ef89
Alexei Chumakov <chumakovalexei@mail.ru>
2021-11-07 21:52:54 +0300

    add: [app.js] app header and some visual for it

--- src/App.js
+++ src/App.js
@@ -3,11 +3,29 @@ import logo2 from './letter-j.png';
 
 
 import './App.css';
+import './Elements.css';
 import React  from 'react';
 
 function App() {
   return (
-    <div className="App">
+    <div className = "App">
+       <div className = "d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
+
+        <p className="h5 my-0 me-md-auto fw-normal">
+          <a className="h3 text-dark text-decoration-none" href="/">JKILA</a>
+        </p>
+        <nav className="my-2 my-md-0 me-md-3" >
+            <a className="p-2 text-dark text-decoration-none" href="/groups" > Teams </a>
+            <a className="p-2 text-dark text-decoration-none" href="/user" > People </a>
+            <a className="p-2 text-dark text-decoration-none" href="/board" > Board </a>
+
+            <a className="h5 p-2 text-decoration-none" href="/user/profile"></a>
+        </nav>
+
+        <a href="/login" className="btn btn-outline-primary ml-5 " >
+            Sign In
+        </a>
+      </div>
       <header className="App-header">
         <img src={logo2} className="App-logo" alt="logo" />
         <p>
