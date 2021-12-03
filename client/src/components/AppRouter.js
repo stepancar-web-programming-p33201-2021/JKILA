9eb96f2fb0c65fe92756b932ae91f573d8d43acd
Трапезин Андрей Александрович <trapezinaa@biocad.ru>
2021-12-03 22:49:19 +0300

    fix: [client] auth bug

--- client/src/components/AppRouter.js
+++ client/src/components/AppRouter.js
@@ -1,37 +1,38 @@
 import React, { useContext } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
+import { observer } from 'mobx-react-lite';
 import { authRoutes, publicRoutes } from '../routes';
 import { Context } from '../index';
 import { LOGIN_ROUTE, WORKSPACES } from '../utils/consts';
 
-const AppRouter = function () {
-  const { user } = useContext(Context);
+const AppRouter = observer(() => {
+    const { user } = useContext(Context);
 
-  console.log(user);
-  return (
-    <Switch>
-      {user.isAuth === true && authRoutes.map(({ path, Component }) => (
-        <Route
-          key={path}
-          path={path}
-          component={Component}
-          exact
-        />
-      ))}
-      {publicRoutes.map(({ path, Component }) => (
-        <Route
-          key={path}
-          path={path}
-          component={Component}
-          exact
-        />
-      ))}
-      { user.isAuth === true
-        ? <Redirect to={WORKSPACES} />
-        : <Redirect to={LOGIN_ROUTE} />}
-    </Switch>
-  );
-};
+    console.log(user);
+    return (
+        <Switch>
+            {user.isAuth === true && authRoutes.map(({ path, Component }) => (
+                <Route
+                    key={path}
+                    path={path}
+                    component={Component}
+                    exact
+                />
+            ))}
+            {publicRoutes.map(({ path, Component }) => (
+                <Route
+                    key={path}
+                    path={path}
+                    component={Component}
+                    exact
+                />
+            ))}
+            { user.isAuth === true
+                ? <Redirect to={WORKSPACES} />
+                : <Redirect to={LOGIN_ROUTE} />}
+        </Switch>
+    );
+});
 
 /* { user.isAuth === true
         ? <Redirect to={WORKSPACES} />
