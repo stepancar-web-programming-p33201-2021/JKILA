03234d606ff0589ad8268fca94c57a7acc6f4a71
Трапезин Андрей Александрович <trapezinaa@biocad.ru>
2021-12-03 22:51:59 +0300

    fix: [client/AppRouter] eslint style

--- client/src/components/AppRouter.js
+++ client/src/components/AppRouter.js
@@ -6,32 +6,32 @@ import { Context } from '../index';
 import { LOGIN_ROUTE, WORKSPACES } from '../utils/consts';
 
 const AppRouter = observer(() => {
-    const { user } = useContext(Context);
+  const { user } = useContext(Context);
 
-    console.log(user);
-    return (
-        <Switch>
-            {user.isAuth === true && authRoutes.map(({ path, Component }) => (
-                <Route
-                    key={path}
-                    path={path}
-                    component={Component}
-                    exact
-                />
-            ))}
-            {publicRoutes.map(({ path, Component }) => (
-                <Route
-                    key={path}
-                    path={path}
-                    component={Component}
-                    exact
-                />
-            ))}
-            { user.isAuth === true
-                ? <Redirect to={WORKSPACES} />
-                : <Redirect to={LOGIN_ROUTE} />}
-        </Switch>
-    );
+  console.log(user);
+  return (
+    <Switch>
+      {user.isAuth === true && authRoutes.map(({ path, Component }) => (
+        <Route
+          key={path}
+          path={path}
+          component={Component}
+          exact
+        />
+      ))}
+      {publicRoutes.map(({ path, Component }) => (
+        <Route
+          key={path}
+          path={path}
+          component={Component}
+          exact
+        />
+      ))}
+      { user.isAuth === true
+        ? <Redirect to={WORKSPACES} />
+        : <Redirect to={LOGIN_ROUTE} />}
+    </Switch>
+  );
 });
 
 /* { user.isAuth === true
